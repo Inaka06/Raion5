2f9f8da38dab99eb9deb0ff1ef9a2d9ae33c6811
HirumiIsna <hilmiisnaeni@student.ub.ac.id>
2025-11-24 23:32:35 +0700

    Commit dash+bg+playerAnim

--- Assets/Scripts/Day 2/Spawner.cs
+++ Assets/Scripts/Day 2/Spawner.cs
@@ -4,14 +4,18 @@ public class Spawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public float spawnInterval = 1f;
-    private float sinceLastSpawn = 0;
+
+    private float sinceLastSpawn = 0f;
+
+    [Header("Spawn Boundary")]
     public float spawnBoundaryY = 4f;
     public float spawnBoundaryX = 10f;
 
     void Update()
     {
         sinceLastSpawn += Time.deltaTime;
-        if(sinceLastSpawn >= spawnInterval)
+
+        if (sinceLastSpawn >= spawnInterval)
         {
             SpawnEnemy();
             sinceLastSpawn = 0f;
@@ -20,15 +24,23 @@ public class Spawner : MonoBehaviour
 
     void SpawnEnemy()
     {
-        if(enemyPrefab == null)
+        if (enemyPrefab == null)
         {
-            Debug.Log("Prefab is null!");
+            Debug.Log("Spawner Error: enemyPrefab is null!");
             return;
         }
+
+        Camera cam = Camera.main;
+
         float randomY = Random.Range(-spawnBoundaryY, spawnBoundaryY);
-        Vector3 spawnPosition = new Vector3(spawnBoundaryX, randomY, 0);
-        GameObject enemy = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+        // Spawn di kanan kamera
+        float xPos = cam.transform.position.x + spawnBoundaryX;
+        float yPos = cam.transform.position.y + randomY;
+
+        Vector3 spawnPosition = new Vector3(xPos, yPos, 0f);
+
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
     }
 
-    
 }
